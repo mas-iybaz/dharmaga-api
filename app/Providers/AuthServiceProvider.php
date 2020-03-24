e4409a462260a006e997ce0fc7a42b6d12676401
mas-iybaz <muhammadiqbal.workspace@gmail.com>
2020-03-24 20:42:52 +0700

    activate auth middleware

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -30,9 +30,17 @@ class AuthServiceProvider extends ServiceProvider
         // should return either a User instance or null. You're free to obtain
         // the User instance via an API token or any other method necessary.
 
+        // $this->app['auth']->viaRequest('api', function ($request) {
+        //     if ($request->input('api_token')) {
+        //         return User::where('api_token', $request->input('api_token'))->first();
+        //     }
+        // });
+
         $this->app['auth']->viaRequest('api', function ($request) {
-            if ($request->input('api_token')) {
-                return User::where('api_token', $request->input('api_token'))->first();
+            if ($request->header('Authorization')) {
+                $part = explode(' ', $request->header('Authorization'));
+
+                return User::where('api_token', $part[1])->first();
             }
         });
     }
